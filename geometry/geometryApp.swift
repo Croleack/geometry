c41434dc607982c7d788845d27c0cc5d0b1bc294
Radik Khaziev <121854191+Croleack@users.noreply.github.com>
2024-02-08 17:55:23 +0500

    Initial Commit

---
+++ geometry/geometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  geometryApp.swift
+//  geometry
+//
+//  Created by Enzhe Gaysina on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct geometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
